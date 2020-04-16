e09e2d775e88fe170d0deaba0c14f238ac6f7d50
Fabricio C. T <fabricioct@outlook.com>
2020-04-16 20:01:55 -0300

    Inti class Logger

---
+++ src/Hedy.Common.Library/Logger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hedy.Common.Library
+{
+   public class Logger
+    {
+        public void Log(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
