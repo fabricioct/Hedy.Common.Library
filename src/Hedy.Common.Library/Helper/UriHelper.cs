1f3f6215bcf50178949c36b880aeab2f7025d89f
Fabrício C. Taglialegna <fabricio.taglialegna@pottencial.com.br>
2021-05-21 16:11:23 -0300

    Adicionado UriHelper

---
+++ src/Hedy.Common.Library/Helper/UriHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace Hedy.Common.Library.Helper
+{
+    public static class UriHelper
+    {
+        public static UriBuilder GetBuilder(Uri uri, params string[] resource)
+        {
+            return GetBuilder(uri, null, resource);
+        }
+
+        public static UriBuilder GetBuilder(Uri uri, object parameters, params string[] resource)
+        {
+            var builder = new UriBuilder(uri)
+            {
+                Path = resource.Aggregate(string.Empty, (a, b) => $"{a}/{b.TrimStart('/', '\\').TrimEnd('/', '\\')}")
+            };
+
+            var query = HttpUtility.ParseQueryString(builder.Query);
+
+            if (parameters == null)
+                return builder;
+
+            foreach (var propertyInfo in parameters.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public))
+                query.Add(propertyInfo.Name, propertyInfo.GetValue(parameters).ToString());
+
+            builder.Query = query.ToString();
+
+            return builder;
+        }
+    }
+}
