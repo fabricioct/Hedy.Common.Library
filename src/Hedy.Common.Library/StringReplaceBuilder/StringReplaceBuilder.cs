892f0bffef8bcb1b642dbedee737fb1b7125e591
Fabrício C. Taglialegna <fabricio.taglialegna@pottencial.com.br>
2021-06-17 16:47:47 -0300

    StringReplaceBuilder

---
+++ src/Hedy.Common.Library/StringReplaceBuilder/StringReplaceBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Hedy.Core.Infrastructure.Rest
+{
+    public class StringReplaceBuilder
+    {
+        private string tagFirst = "#{";
+        private string tagLast = "}#";
+        private string template = string.Empty;
+
+
+        public StringReplaceBuilder()
+        {
+        }
+
+        public StringReplaceBuilder AddTagFirst(string tagFirst)
+        {
+            this.tagFirst = tagFirst;
+            return this;
+        }
+
+        public StringReplaceBuilder AddTagLast(string tagLast)
+        {
+            this.tagLast = tagLast;
+            return this;
+        }
+
+        public StringReplaceBuilder AddTamplate(string template)
+        {
+            this.template = template;
+            return this;
+        }
+
+    }
+}
